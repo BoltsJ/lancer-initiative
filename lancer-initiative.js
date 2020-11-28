3671c9df8bbe740934e7ff1e25d354c64952a3ad
Joe Nosie <joenosie@protonmail.com>
2020-11-27 19:55:01 -0600

    Initial libWrapper migration
    Initial changes to use libWrapper to make the module more robust to core
    foundry changes and to help with resolving potential conflicts for
    overridden core methods.

--- lancer-initiative.js
+++ lancer-initiative.js
@@ -1,186 +1,169 @@
-class LancerInitiative {
-    static async setup() {
-        console.log(
-            "lancer-initiative | Initializing LANCER Initiative Module"
-        );
+import {libWrapper} from './module/shim.js';
 
-        await LancerInitiative.registerSettings();
+async function setup() {
+  libWrapper.register('lancer-initiative', 'Combat.prototype._sortCombatants', function (w, a, b) {
+    let r = sortCombatants(a, b);
+    if (r === 0) r = w.apply(this, [a, b]);
+    return r;
+  });
 
-        Combat.prototype._sortCombatants = LancerInitiative.sortCombatants;
-        CombatTracker.prototype._getEntryContextOptions =
-            LancerInitiative.getEntryContextOptions;
-    }
-
-    static registerSettings() {
-        game.settings.register("lancer-initiative", "pc-col", {
-            name: "Player button color",
-            hint: "Default: $44abe0",
-            scope: "world",
-            config: true,
-            type: String,
-            default: "#44abe0"
-        });
-        game.settings.register("lancer-initiative", "nu-col", {
-            name: "Neutral button color",
-            hint: "Default: #146464",
-            scope: "world",
-            config: true,
-            type: String,
-            default: "#146464"
-        });
-        game.settings.register("lancer-initiative", "en-col", {
-            name: "Enemy button color",
-            hint: "Default: #d98f30",
-            scope: "world",
-            config: true,
-            type: String,
-            default: "#d98f30"
-        });
-        game.settings.register("lancer-initiative", "xx-col", {
-            name: "Inactive button color",
-            hint: "Default: #444444",
-            scope: "world",
-            config: true,
-            type: String,
-            default: "#444444"
-        });
-        game.settings.register("lancer-initiative", "icon", {
-            name: "Action Icon",
-            hint: "CSS classes to define the activation icon; li-icon, li-icon-large, and li-icon-xlarge are defined to increase the size if needed",
-            scope: "world",
-            config: true,
-            type: String,
-            default: "cci cci-activate li-icon-large"
-        });
-        game.settings.register("lancer-initiative", "act-sort-last", {
-            name: "Activated units last",
-            hint: "Moves units that have activated to the end of the tracker",
-            scope: "world",
-            config: true,
-            type: Boolean,
-            default: false
-        });
-    }
-
-    static sortCombatants(a, b) {
-        //DUMMYs first ;)
-        if ( a.flags?.dummy === true ) return -1;
-        if ( b.flags?.dummy === true ) return 1;
-
-        // Move inactive to the bottom
-        if ( game.settings.get("lancer-initiative", "act-sort-last") ) {
-            const act_a = a.flags.activations?.value === 0;
-            const act_b = b.flags.activations?.value === 0;
-            const act_c = act_a - act_b;
-            if ( act_c !==0 ) return act_c;
-        }
-        // Sort by Players then Neutrals then Hostiles
-        const dc = b.token.disposition - a.token.disposition;
-        if ( dc !== 0 ) return dc;
-        // Reverse initiative sorting; the rest is default foundry
-        const ia = Number.isNumeric(a.initiative) ? a.initiative : 9999;
-        const ib = Number.isNumeric(b.initiative) ? b.initiative : 9999;
-        let ci = ia - ib;
-        if ( ci !== 0 ) return ci;
-        let [an, bn] = [a.token?.name || "", b.token?.name || ""];
-        let cn = an.localeCompare(bn);
-        if ( cn !== 0 ) return cn;
-        return a.tokenId - b.tokenId;
-    }
-
-    static getEntryContextOptions() {
-        return [ // TODO: Fix these to work for the new data setting
-            {
-                name: "Add Activation",
-                icon: '<i class="fas fa-plus"></i>',
-                callback:  async (li) => {
-                    const combatant = this.combat.getCombatant(li.data('combatant-id'));
-                    let max = combatant.flags.activations.max + 1;
-                    await this.combat.updateCombatant({
-                        _id: combatant._id,
-                        "flags.activations.max": max
-                    });
-                }
-            },
-            {
-                name: "Remove Activation",
-                icon: '<i class="fas fa-minus"></i>',
-                callback:  async (li) => {
-                    const combatant = this.combat.getCombatant(li.data('combatant-id'));
-                    let max = combatant.flags.activations.max - 1;
-                    let cur = clampNumber(combatant.flags.activations.value, 0, max > 0 ? max : 1);
-                    await this.combat.updateCombatant({
-                        _id: combatant._id,
-                        "flags.activations.max": max > 0 ? max : 1,
-                        "flags.activations.value": cur
-                    });
-                }
-            },
-            {
-                name: "Undo Activation",
-                icon: '<i class="fas fa-undo"></i>',
-                callback: li => {
-                    const combatant = this.combat.getCombatant(li.data('combatant-id'));
-                    let max = combatant.flags.activations.max;
-                    let cur = clampNumber(combatant.flags.activations.value+1, 0, max > 0 ? max : 1);
-                    this.combat.updateCombatant({
-                        _id: combatant._id,
-                        "flags.activations.value": cur
-                    });
-                }
-            },
-            {
-                name: "COMBAT.CombatantUpdate",
-                icon: '<i class="fas fa-edit"></i>',
-                callback: this._onConfigureCombatant.bind(this)
-            },
-            {
-                name: "COMBAT.CombatantRemove",
-                icon: '<i class="fas fa-skull"></i>',
-                callback: li => this.combat.deleteCombatant(li.data('combatant-id'))
-            }
-        ];
-    }
-
-    static handleCreateCombat(combat, options, userId) {
-        if (game.user.isGM) combat.createCombatant({
-            name: "DUMMY",
-            flags: { dummy: true },
-            hidden: true
-        });
-    }
-
-    static handleUpdateCombat(combat, changed, options, userId) {
-        if (! game.user.isGM ) return;
-        if ("round" in changed) {
-            combat.combatants.map(c =>
-                combat.updateCombatant({
-                    _id: c._id,
-                    "flags.activations.value": c.defeated ? 0 : c.flags.activations?.max
-                })
-            );
-        }
-    }
-
-    static handleCreateCombatant(combat, combatant, options, userId) {
-        if (! game.user.isGM ) return;
-        if (combatant.tokenId === undefined) return;
-        // AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-        let a = canvas.tokens.get(combatant.tokenId).actor.data.data?.activations;
-        combat.updateCombatant({
+  libWrapper.register('lancer-initiative', 'CombatTracker.prototype._getEntryContextOptions', function(w) {
+    let m = [
+      {
+        name: "Add Activation",
+        icon: '<i class="fas fa-plus"></i>',
+        callback:  async (li) => {
+          const combatant = this.combat.getCombatant(li.data('combatant-id'));
+          let max = combatant.flags.activations.max + 1;
+          await this.combat.updateCombatant({
             _id: combatant._id,
-            flags: {
-                activations: {
-                    value: 0,
-                    max: a === undefined ? 1 : a
-                }
-            }
-        });
-    }
+            "flags.activations.max": max
+          });
+        }
+      },
+      {
+        name: "Remove Activation",
+        icon: '<i class="fas fa-minus"></i>',
+        callback:  async (li) => {
+          const combatant = this.combat.getCombatant(li.data('combatant-id'));
+          let max = combatant.flags.activations.max - 1;
+          let cur = clampNumber(combatant.flags.activations.value, 0, max > 0 ? max : 1);
+          await this.combat.updateCombatant({
+            _id: combatant._id,
+            "flags.activations.max": max > 0 ? max : 1,
+            "flags.activations.value": cur
+          });
+        }
+      },
+      {
+        name: "Undo Activation",
+        icon: '<i class="fas fa-undo"></i>',
+        callback: li => {
+          const combatant = this.combat.getCombatant(li.data('combatant-id'));
+          let max = combatant.flags.activations.max;
+          let cur = clampNumber(combatant.flags.activations.value+1, 0, max > 0 ? max : 1);
+          this.combat.updateCombatant({
+            _id: combatant._id,
+            "flags.activations.value": cur
+          });
+        }
+      }
+    ];
+    m.push(...w.call().filter(i => i.name !== "COMBAT.CombatantReroll"))
+    return m;
+  });
+
 }
 
-Hooks.on("init", LancerInitiative.setup);
-Hooks.once("ready", () => { // Detect and recover from Foundry deciding that it doesn't want to save data.
+function registerSettings() {
+  console.log("lancer-initiative | Initializing LANCER Initiative Module");
+  game.settings.register("lancer-initiative", "pc-col", {
+    name: "Player button color",
+    hint: "Default: $44abe0",
+    scope: "world",
+    config: true,
+    type: String,
+    default: "#44abe0"
+  });
+  game.settings.register("lancer-initiative", "nu-col", {
+    name: "Neutral button color",
+    hint: "Default: #146464",
+    scope: "world",
+    config: true,
+    type: String,
+    default: "#146464"
+  });
+  game.settings.register("lancer-initiative", "en-col", {
+    name: "Enemy button color",
+    hint: "Default: #d98f30",
+    scope: "world",
+    config: true,
+    type: String,
+    default: "#d98f30"
+  });
+  game.settings.register("lancer-initiative", "xx-col", {
+    name: "Inactive button color",
+    hint: "Default: #444444",
+    scope: "world",
+    config: true,
+    type: String,
+    default: "#444444"
+  });
+  game.settings.register("lancer-initiative", "icon", {
+    name: "Action Icon",
+    hint: "CSS classes to define the activation icon; li-icon, li-icon-large, and li-icon-xlarge are defined to increase the size if needed",
+    scope: "world",
+    config: true,
+    type: String,
+    default: "cci cci-activate li-icon-large"
+  });
+  game.settings.register("lancer-initiative", "act-sort-last", {
+    name: "Activated units last",
+    hint: "Moves units that have activated to the end of the tracker.  WARNING: Enabling this can prevent some modules from detecting turn changes",
+    scope: "world",
+    config: true,
+    type: Boolean,
+    default: false
+  });
+}
+
+function sortCombatants(a, b) {
+  //DUMMYs first ;)
+  if ( a.flags?.dummy === true ) return -1;
+  if ( b.flags?.dummy === true ) return 1;
+  // Move inactive to the bottom
+  if ( game.settings.get("lancer-initiative", "act-sort-last") ) {
+    const act_a = a.flags.activations?.value === 0;
+    const act_b = b.flags.activations?.value === 0;
+    const act_c = act_a - act_b;
+    if ( act_c !==0 ) return act_c;
+  }
+  // Sort by Players then Neutrals then Hostiles
+  const dc = b.token.disposition - a.token.disposition;
+  if ( dc !== 0 ) return dc;
+  return 0; //fallback to foundry
+}
+
+function handleCreateCombat(combat, options, userId) {
+  if (game.user.isGM) combat.createCombatant({
+    name: "DUMMY",
+    flags: { dummy: true },
+    hidden: true
+  });
+}
+
+function handleUpdateCombat(combat, changed, options, userId) {
+  if (! game.user.isGM ) return;
+  if ("round" in changed) {
+    combat.combatants.map(c =>
+      combat.updateCombatant({
+        _id: c._id,
+        "flags.activations.value": c.defeated ? 0 : c.flags.activations?.max
+      })
+    );
+  }
+}
+
+function handleCreateCombatant(combat, combatant, options, userId) {
+  if (! game.user.isGM ) return;
+  if (combatant.tokenId === undefined) return;
+  // AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+  let a = canvas.tokens.get(combatant.tokenId).actor.data.data?.activations;
+  combat.updateCombatant({
+    _id: combatant._id,
+    flags: {
+      activations: {
+        value: 0,
+        max: a === undefined ? 1 : a
+      }
+    }
+  });
+}
+
+//Hooks.once("init", registerSettings);
+Hooks.once("init", () => { // Detect and recover from Foundry deciding that it doesn't want to save data.
+  setup();
+  registerSettings();
   if (! game.user.isGM ) return;
   game.combats.map(c => {
     c.combatants.map(t => {
@@ -200,9 +183,9 @@ Hooks.once("ready", () => { // Detect and recover from Foundry deciding that it 
   });
 });
 
-Hooks.on("createCombat", LancerInitiative.handleCreateCombat);
-Hooks.on("updateCombat", LancerInitiative.handleUpdateCombat);
-Hooks.on("createCombatant", LancerInitiative.handleCreateCombatant);
+Hooks.on("createCombat", handleCreateCombat);
+Hooks.on("updateCombat", handleUpdateCombat);
+Hooks.on("createCombatant", handleCreateCombatant);
 
 Hooks.on("renderCombatTracker", async (app, html, data) => {
     html.find(".combatant").each((i, element) => {
