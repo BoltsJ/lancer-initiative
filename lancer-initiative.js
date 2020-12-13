41f3bd8c33dd631b00abe7a3722aabbb9f7d06f6
Joe <joenosie@protonmail.com>
2020-12-12 21:03:38 -0600

    Revert "Adds localization support and Spanish strings"

--- lancer-initiative.js
+++ lancer-initiative.js
@@ -5,32 +5,32 @@ function registerSettings() {
   console.log("lancer-initiative | Initializing Lancer Initiative Module");
 
   game.settings.register("lancer-initiative", "pc-col", {
-    name: "LancerInitiative.Settings.PlayerButtonColor",
-    hint: "LancerInitiative.Settings.PlayerButtonColorHint",
+    name: game.i18n.localize("LANCERINITIATIVE.PCColor"),
+    hint: "Default: $44abe0",
     scope: "world",
     config: true,
     type: String,
     default: "#44abe0",
   });
   game.settings.register("lancer-initiative", "nu-col", {
-    name: "LancerInitiative.Settings.NeutralButtonColor",
-    hint: "LancerInitiative.Settings.NeutralButtonColorHint",
+    name: game.i18n.localize("LANCERINITIATIVE.NeutralColor"),
+    hint: "Default: #146464",
     scope: "world",
     config: true,
     type: String,
     default: "#146464",
   });
   game.settings.register("lancer-initiative", "en-col", {
-    name: "LancerInitiative.Settings.EnemyButtonColor",
-    hint: "LancerInitiative.Settings.EnemyButtonColorHint",
+    name: game.i18n.localize("LANCERINITIATIVE.EnemyColor"),
+    hint: "Default: #d98f30",
     scope: "world",
     config: true,
     type: String,
     default: "#d98f30",
   });
   game.settings.register("lancer-initiative", "xx-col", {
-    name: "LancerInitiative.Settings.InactiveButtonColor",
-    hint: "LancerInitiative.Settings.InactiveButtonColorHint",
+    name: game.i18n.localize("LANCERINITIATIVE.DoneColor"),
+    hint: "Default: #444444",
     scope: "world",
     config: true,
     type: String,
@@ -47,16 +47,16 @@ function registerSettings() {
   }
 
   game.settings.register("lancer-initiative", "icon", {
-    name: "LancerInitiative.Settings.ActionIcon",
-    hint: "LancerInitiative.Settings.ActionIconHint",
+    name: game.i18n.localize("LANCERINITIATIVE.Icon"),
+    hint: game.i18n.localize("LANCERINITIATIVE.IconDesc"),
     scope: "world",
     config: true,
     type: String,
     default: def_icon,
   });
   game.settings.register("lancer-initiative", "act-sort-last", {
-    name: "LancerInitiative.Settings.ActivatedUnitsLast",
-    hint: "LancerInitiative.Settings.ActivatedUnitsLastHint",
+    name: game.i18n.localize("LANCERINITIATIVE.ActivatedLast"),
+    hint: game.i18n.localize("LANCERINITIATIVE.ActivatedLastDesc"),
     scope: "world",
     config: true,
     type: Boolean,
--- module/lancer-combat-tracker.js
+++ module/lancer-combat-tracker.js
@@ -28,7 +28,7 @@ export class LancerCombatTracker extends CombatTracker {
   _getEntryContextOptions() {
     let m = [
       {
-        name: "LancerInitiative.AddActivation",
+        name: game.i18n.localize("LANCERINITIATIVE.AddActivation"),
         icon: '<i class="fas fa-plus"></i>',
         callback: async li => {
           const combatant = this.combat.getCombatant(li.data("combatant-id"));
@@ -40,7 +40,7 @@ export class LancerCombatTracker extends CombatTracker {
         },
       },
       {
-        name: "LancerInitiative.RemoveActivation",
+        name: game.i18n.localize("LANCERINITIATIVE.RemoveActivation"),
         icon: '<i class="fas fa-minus"></i>',
         callback: async li => {
           const combatant = this.combat.getCombatant(li.data("combatant-id"));
@@ -54,7 +54,7 @@ export class LancerCombatTracker extends CombatTracker {
         },
       },
       {
-        name: "LancerInitiative.UndoActivation",
+        name: game.i18n.localize("LANCERINITIATIVE.UndoActivation"),
         icon: '<i class="fas fa-undo"></i>',
         callback: li => {
           const combatant = this.combat.getCombatant(li.data("combatant-id"));
